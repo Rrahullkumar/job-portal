f28998f9b582ab5fc75e76fa31f341a24d12e932
Rrahullkumar <rahulkr811844@gmail.com>
2025-01-31 19:24:17 +0530

    modified server.js

--- server/server.js
+++ server/server.js
@@ -29,6 +29,7 @@ const PORT = process.env.PORT || 5000
 
 Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT , ()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+// app.listen(PORT , ()=>{
+//     console.log(`server is running on port ${PORT}`)
+// })
+export default app
